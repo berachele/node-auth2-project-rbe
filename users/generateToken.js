cc35ad93a5b9d51dc566eebb30bae62b273ebf68
Rachele Edwards <dot.com1472@gmail.com>
2020-05-19 10:47:48 -0600

    deleted .env file because not working, used config/vars technique. register and login is successful

--- users/generateToken.js
+++ users/generateToken.js
@@ -1,12 +1,16 @@
+const configVars = require('../config/vars')
+const jwt = require('jsonwebtoken')
+
 module.exports = function generateToken(user) {
     const payload = {
         subject: user.id,
         username: user.username,
         department: user.department
-    },
+    }
+
     const options = {
         expiresIn: '1h'
     } 
 
-    return jwt.sign(payload, JWT_SECRET, options)
+    return jwt.sign(payload, configVars.jwtSecret, options)
 }
